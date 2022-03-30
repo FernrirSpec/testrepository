a32e3b3220e128c1ec842ec9bfeecf5f4ba8f264
stfreon <Voloshyn.D.A@nmu.one>
2022-03-30 13:09:24 +0300

    delete

--- src/com/company/JThread.java
+++ src/com/company/JThread.java
@@ -20,15 +20,6 @@ public class JThread implements Runnable{
         Path path = Paths.get("C:\\Users\\St.Freon\\Desktop\\some_log.log");
         String filter ="! Can't find texture";
 
-        try(Stream<String> lines1 = Files.lines(path))
-        {
-
-            Stream<String> res = lines1.filter(string -> string.contains(filter))
-                    .map(s -> s.substring(33,s.length()-1));
-
-            try(FileOutputStream fos = new FileOutputStream("D:\\programming\\output.txt",true))
-            {
-
                 List<String> targetList;
                 targetList = res.collect(Collectors.toList());
 
